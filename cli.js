6384dcc51228c912c49b0f27927fee42ad7c24bd
Patrick R <patrick.rabier@gmail.com>
2020-12-07 12:39:11 +0800

    :children_crossing: Improve CLI with commander

--- cli.js
+++ cli.js
@@ -1,64 +1,66 @@
 #!/usr/bin/env node
 
-const { execSync, spawnSync }  = require('child_process');
+const { execSync, spawnSync }   = require('child_process');
+const { program }               = require('commander');
+const debug                     = require('debug')
+const pkg                       = require('./package.json')
 
-if (process.argv.length !== 4) {
-  console.error(`
-    git_on_change will run the specified command only if the type of extension specified was changed in the last commit
-    
-    Usage:
-      git_on_change [extensions] [command]
+const info        = debug('info');
+const error       = debug('fatal');
+const commaSplit  = (value) => value.split(',');
+const upper       = (s = "") => s.toUpperCase()
 
-      Example:
-        git_on_change JS "npm run test" # Will only run tests if javascript has changed
-        git_on_change -JS "echo 'NO JS files were changed'"
-  `)
-  process.exit(1);
-}
-
-const files       = execSync(`git --no-pager diff --name-only HEAD HEAD~1`).toString().split('\n');
-const extensions  = {}
-
-const identity = (i) => i
-
-const upper = (s) => s.toUpperCase()
-
-files.forEach(f => {
-  const split = f.split('.')
-  const ext   = split[split.length - 1];
-  
-  extensions[ext] = true;
-});
-
-const changes =Object
-  .keys(extensions)
-  .filter(identity)
-  .map(upper)
-  
-const extensionsToListen  = process.argv[2];
-const commandToRun        = process.argv[3];
-
-let shouldTrigger = false;
-
-console.info('=== Detected the following file type changes', changes);
-console.info('=== Comparing with', extensionsToListen);
-
-extensionsToListen.split(',').forEach(input => {
-  const not       = input[0] == '-';
-  const ext       = input.toUpperCase().replace(/-/g, '');
-  const inChanges = changes.find(c => c === ext);
-
-  if (!shouldTrigger) {
-    shouldTrigger = (inChanges && !not) || (!inChanges && not);
+const exit = (code) => {
+  if (code === 0) {
+    info(`Exiting with status ${code}`)
+  } else {
+    error(`Exiting with status ${code}`)
   }
-});
-
-if (shouldTrigger) {
-  console.info(`=== Running ${commandToRun}`)
-  const res   = spawnSync(commandToRun, { stdio: 'inherit' });
-  const code  = res.status === null ? 1 : 0;
-
-  console.info(`=== Exiting with status ${code}`)
-  process.exit(code);
+  process.exit(0);
 }
 
+program
+  .version(pkg.version)
+  .option('-e, --extensions <extensions>', 'comma separated list of extensions', commaSplit)
+  .command('run <cmd>')
+  .action((commandToRun) => {
+    console.log(program.extensions)
+    const extensions  = program.extensions || [];
+    const files       = execSync(`git --no-pager diff --name-only HEAD HEAD~1`).toString().split('\n');
+    const changes     = files.reduce((arr, f) => {
+      const split = f.split('.')
+      const ext   = upper(split[split.length - 1])
+      return (ext && arr.indexOf(ext) < 0) ? [...arr, ext] : arr;
+    }, []);
+
+    info('Detected the following file type changes', changes);
+    info('Comparing with', extensions);
+
+    const shouldTrigger = extensions.find(extIn => {
+      const not       = extIn[0] == '-';
+      const ext       = extIn.toUpperCase().replace(/-/g, '');
+      const inChanges = changes.find(c => c === ext);
+    
+      return (inChanges && !not) || (!inChanges && not);
+    });
+
+    if (shouldTrigger) {
+      info(`Running ${commandToRun}`)
+      const { status } = spawnSync(commandToRun, { stdio: 'inherit' });
+      exit(status === null ? 1 : status);
+    }
+
+    exit(0)
+  });
+
+program.on('--help', () => {
+  console.log(`
+    Examples:
+
+    $> git_on_change --extensions JS   "echo 'Some JS files were changed'
+    $> git_on_change --extensions -JS  "echo 'No JS files were changed'"
+  `)
+});
+  
+
+program.parse(process.argv);
